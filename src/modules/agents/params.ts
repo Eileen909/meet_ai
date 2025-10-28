6d687112bf17a78e122b2a66f86b729d2b7d7848
Eileen <eileenmthayil@gmail.com>
2025-10-28 10:18:31 +0300

    13: agents filters

---
+++ src/modules/agents/params.ts
@@ -0,0 +1,9 @@
+import { parseAsInteger, parseAsString } from "nuqs/server";
+
+
+import { DEFAULT_PAGE } from "@/constants";
+
+export const filterSearchParams = {
+    search: parseAsString.withDefault("").withOptions({clearOnDefault: true }),
+    page: parseAsInteger.withDefault(DEFAULT_PAGE).withOptions({clearOnDefault: true }),
+};
